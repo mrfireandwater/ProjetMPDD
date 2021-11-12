4d42d83c29a2d81066acf9cf55b29d595ec84baf
helonolf <72448284+helonolf@users.noreply.github.com>
2021-11-12 14:42:41 +0100

    Update Sensors_template.ino

--- Sensors_template.ino
+++ Sensors_template.ino
@@ -3,22 +3,22 @@
 Servo servo1;     //Wight visor (inner visor)
 Servo servo2;     //Sun visor (outer visor)
 
-const int analogRain = A0;    //Analog in pin of rain sensor  
-const int analogDist = A1;    //Analog in pin of distance sensor  
-const int speedPin = 11;      //In pin of speed sensor  
-const int lightPin = 12;      //In pin of light sensor 
+const int analogRain = A0;  //TODO  //Analog in pin of rain sensor  
+const int analogDist = A1;  //TODO  //Analog in pin of distance sensor  
+const int speedPin = 11;    //TODO  //In pin of speed sensor  
+const int lightPin = 12;    //TODO  //In pin of light sensor 
 
 int rainValue, distValue, speedValue, lightValue;      //Value of the different sensors
 int rainLimit, distLimit, speedLimit, lightLimit;      //Limit of the different sensors
 
-int openPos=60;         //Open visor position
-int closePos;          //Closed visor position
+int openPos=60;     //TODO    //Open visor position
+int closePos;                 //Closed visor position
 
 int r ;         //Rayon de la courbe des lunettes   
 
 void setup() {
-  servo1.attach(9);     // attaches the servo1 on pin 9 to the servo object 
-  servo2.attach(10);    // attaches the servo2 on pin 10 to the servo object 
+  servo1.attach(9);     //TODO: mettre la bonne pin    // attaches the servo1 on pin 9 to the servo object 
+  servo2.attach(10);    //TODO: mettre la bonne pin    // attaches the servo2 on pin 10 to the servo object 
   Serial.begin(9600);   // initialize serial communications at 9600 bps:
 }
 
