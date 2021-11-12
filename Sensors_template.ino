4d44d8bbf1045d5a8279b10005f1319f496ecfed
helonolf <72448284+helonolf@users.noreply.github.com>
2021-11-12 12:34:10 +0100

    Update Sensors_template.ino

--- Sensors_template.ino
+++ Sensors_template.ino
@@ -1,27 +1,27 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo servo1;     //Weight visor (inner visor)
+Servo servo2;     //Sun visor (outer visor)
 
 const int analogRain = A0;    //Analog in pin of rain sensor  
 const int analogDist = A1;    //Analog in pin of distance sensor  
 const int speedPin = 11;      //In pin of speed sensor  
 const int lightPin = 12;      //In pin of light sensor 
 
-const int servo1 = 9;     //Controls wight visor (inner visor)
-const int servo2 = 10;    //Controls sun visor (outer visor)
-
 int rainValue, distValue, speedValue, lightValue;      //Value of the different sensors
 int rainLimit, distLimit, speedLimit, lightLimit;      //Limit of the different sensors
 
-int pos1=60;         //Open visor position
-int pos2=180;        //Closed visor position
+int openPos=60;         //Open visor position
+int closePos=180;       //Closed visor position
 
 int pos;
 
 void setup() {
+  servo1.attach(9);     // attaches the servo1 on pin 9 to the servo object 
+  servo1 object
+  servo2.attach(10);    // attaches the servo2 on pin 10 to the servo object 
+  servo2 object
   Serial.begin(9600);   // initialize serial communications at 9600 bps:
-  myservo.attach(9);    // attaches the servo1 on pin 9 to the servo object 
-  myservo.attach(10);    // attaches the servo2 on pin 10 to the servo object 
 }
 
 void loop() {
@@ -31,17 +31,33 @@ void loop() {
   speedValue = // TODO;
   lightValue = //TODO;
 
-  rainLimit = 600;      //<600:MOUILLE, >600:SEC
-  /*distLimit = #TODO;
-  speedLimit = #TODO;
-  lightLimit = #TODO;*/   //>lightlimit:SUN, <lightLimit<NO SUN
+  rainLimit = 600;      //<600:RAIN, >600:NO RAIN
+  distLimit = //TODO;
+  speedLimit = //TODO;   //>lightlimit:WIND, <lightLimit<NO WIND
+  lightLimit = //TODO;   //>lightlimit:SUN, <lightLimit<NO SUN
 
-  if(rainValue<rainLimit and lightValue>lightLimit){
-    myservo.write(pos1);
+  //Rain+Sun+Wind or Rain+Sun or Wind+Sun
+  if(rainValue<rainLimit && (lightValue>lightLimit || speedValue>speedLimit){
+    servo1.write(closePos);
+    servo2.write(closePos);
     delay(100);
   }
+  //No Sun: Rain+Wind or Rain or Wind
+  if(lightValue<lightLimit && (lightValue>lightLimit || speedValue>speedLimit){
+    servo1.write(closePos);
+    servo2.write(openPos);
+    delay(100);
+  }
+  //Sun, No Wind, No Rain
+  if(lightValue>lightLimit && speedValue<speedLimit && rainValue>rainLimit){
+    servo1.write(openPos);
+    servo2.write(closePos);
+    delay(100);
+  }
+  //No Sun, No Wind, No Rain
   else{
-    myservo.write(pos2);
+    servo1.write(openPos);
+    servo2.write(openPos);
     delay(100);
   }
   
